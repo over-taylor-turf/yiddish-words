0a2f4ce627c1fcce815929230d12c747e9b404da
Taylor <taylorpollioverturf@gmail.com>
2021-12-20 09:16:22 -0800

    update model

--- api/models/suggested_word.py
+++ api/models/suggested_word.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class SuggestedWord(models.Model):
+    email = models.CharField(max_length=255)
     word = models.CharField(max_length=255)
     phonetic_spelling = models.CharField(max_length=255)
     definition = models.CharField(max_length=255)
@@ -8,4 +9,4 @@ class SuggestedWord(models.Model):
     bubbe = models.CharField(max_length=255)
 
     def __str__(self):
-            return f"{self.word} is pronounced as '{self.phonetic_spelling}' and means '{self.definition}'. An example sentence would be, '{self.example_sentence}' This word has been added in honor of Bubbe {self.bubbe}."
+            return f"Proposed by {self.email}: {self.word} is pronounced as '{self.phonetic_spelling}' and means '{self.definition}'. An example sentence would be, '{self.example_sentence}' This word has been added in honor of Bubbe {self.bubbe}."
